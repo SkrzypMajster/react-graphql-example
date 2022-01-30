723bc52d1bf9a4bf93462979a594ce7d319d00a8
Dawid Skrzypczyk <skrzypmajster@gmail.com>
2022-01-30 15:51:56 +0100

    Make characters list card component clickable with redirection to character details view

--- src/components/CharactersList/CharactersList.tsx
+++ src/components/CharactersList/CharactersList.tsx
@@ -3,6 +3,8 @@ import {Box, Pagination} from "@mui/material";
 
 import {CharacterListItem} from "../CharacterListItem/CharacterListItem";
 import {CharactersListProps} from "./CharactersList.types";
+import {Link} from "react-router-dom";
+import {CharactersListItem} from "../../hooks/useCharacters/useCharacters.types";
 
 export const CharactersList = ({characters, pagesCount, page, onChangePage}: CharactersListProps) => {
     const handleOnChange = (event: ChangeEvent<unknown>, value: number) => {
@@ -11,16 +13,22 @@ export const CharactersList = ({characters, pagesCount, page, onChangePage}: Cha
 
     return (
         <Box>
-            <Box sx={{ display: 'flex', gap: 3, flexWrap: 'wrap'}}>
+            <Box sx={{display: 'flex', gap: 3, flexWrap: 'wrap'}}>
                 {
-                    characters.map((character: { name: string, image: string }) => (
-                        <CharacterListItem key={character.name} name={character.name} image={character.image}/>
+                    characters.map((character: CharactersListItem) => (
+                        <Link
+                            to={`/characters/${character.id}`}
+                            key={character.name}
+                            style={{textDecoration: 'none', color: '#000'}}
+                        >
+                            <CharacterListItem name={character.name} image={character.image}/>
+                        </Link>
                     ))
                 }
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+            <Box sx={{display: 'flex', justifyContent: 'center'}}>
                 <Pagination
-                    sx={{ marginTop: 4 }}
+                    sx={{marginTop: 4}}
                     variant="outlined"
                     shape="rounded"
                     count={pagesCount}
