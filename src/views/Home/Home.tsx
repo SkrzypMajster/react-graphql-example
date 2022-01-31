17f9d65fc93c35265605a6126a3ee911b37968a8
Dawid Skrzypczyk <skrzypmajster@gmail.com>
2022-01-31 21:10:32 +0100

    Connect all tiles in Homepage view component

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,3 +1,11 @@
+import {Box} from "@mui/material";
+
+import {HomepageTile} from "../../components/HomepageTile/HomepageTile";
+
 export const Home = () => (
-    <div>Home page is working!</div>
+    <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+        <HomepageTile path="/characters" title="Characters" subtitle="Check characters list and it's details." />
+        <HomepageTile path="/episodes" title="Episodes" subtitle="Check episodes list and it's details." />
+        <HomepageTile path="/locations" title="Locations" subtitle="Check locations list and it's details." />
+    </Box>
 );
