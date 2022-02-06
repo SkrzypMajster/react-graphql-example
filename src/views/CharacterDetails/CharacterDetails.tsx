e0a7c3f10724c2cde3ba9b482db784400baa6c19
Dawid Skrzypczyk <skrzypmajster@gmail.com>
2022-02-06 01:01:12 +0100

    Add GetCharacter query file, replace useCharacter hook with auto-generated one

--- src/views/CharacterDetails/CharacterDetails.tsx
+++ src/views/CharacterDetails/CharacterDetails.tsx
@@ -2,12 +2,14 @@ import {Link, useParams} from "react-router-dom";
 import {Alert, AlertTitle, Avatar, Box, Button, Divider, LinearProgress, Typography} from "@mui/material";
 import {ArrowBack} from "@mui/icons-material";
 
-import {useCharacter} from "../../hooks/useCharacter/useCharacter";
 import {BaseLink} from "../../components/BaseLink/BaseLink";
+import {useGetCharacterQuery} from "../../graphql";
 
 export const CharacterDetails = () => {
     const {id} = useParams();
-    const {loading, error, data} = useCharacter({id: Number(id)});
+    const {loading, error, data} = useGetCharacterQuery({
+        variables: {id: id || ''}
+    })
 
     if (loading) {
         return <LinearProgress/>;
@@ -25,45 +27,45 @@ export const CharacterDetails = () => {
     return (
         <div>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                <Avatar
-                    src={data.character.image}
-                    alt={`${data.character.name} image`}
-                    sx={{width: 200, height: 200}}
-                />
-                <Typography variant="h5" sx={{marginTop: 1}}>{data.character.name}</Typography>
-                <Typography variant="subtitle1" sx={{marginBottom: 4}}>{data.character.species}</Typography>
+                {data.character?.image && <Avatar
+                  src={data.character.image}
+                  alt={`${data.character.name} image`}
+                  sx={{width: 200, height: 200}}
+                />}
+                <Typography variant="h5" sx={{marginTop: 1}}>{data.character?.name || '-'}</Typography>
+                <Typography variant="subtitle1" sx={{marginBottom: 4}}>{data.character?.species || '-'}</Typography>
             </Box>
             <Divider/>
             <Typography variant="h5" sx={{marginTop: 2}}>Character details:</Typography>
             <Box sx={{marginTop: 1, marginBottom: 1}}>
                 <Typography variant="body1">
                     <strong>Gender: </strong>
-                    <span>{data.character.gender}</span>
+                    <span>{data.character?.gender || '-'}</span>
                 </Typography>
                 <Typography variant="body1">
                     <strong>Status: </strong>
-                    <span>{data.character.status}</span>
+                    <span>{data.character?.status || '-'}</span>
                 </Typography>
                 <Typography variant="body1">
                     <strong>Origin: </strong>
                     {
-                        data.character.origin.id ?
+                        data.character?.origin?.id ?
                             (
                                 <Link to={`/locations/${data.character.origin.id}`}>
                                     {data.character.origin.name}
                                 </Link>
-                            ) : <span>{data.character.origin.name}</span>
+                            ) : <span>{data.character?.origin?.name || '-'}</span>
                     }
                 </Typography>
                 <Typography variant="body1">
                     <strong>Location: </strong>
                     {
-                        data.character.location.id ?
+                        data.character?.location?.id ?
                             (
                                 <Link to={`/locations/${data.character.location.id}`}>
                                     {data.character.location.name}
                                 </Link>
-                            ) : <span>{data.character.location.name}</span>
+                            ) : <span>{data.character?.location?.name || '-'}</span>
                     }
                 </Typography>
             </Box>
@@ -71,12 +73,13 @@ export const CharacterDetails = () => {
             <Typography variant="h5" sx={{marginTop: 2}}>Episodes:</Typography>
             <ul>
                 {
-                    data.character.episode.map((episode) => (
-                        <Link to={`/episodes/${episode.id}`} key={episode.id}>
+                    data.character?.episode.map((episode) => (
+                        episode?.id? <Link to={`/episodes/${episode.id}`} key={episode.id}>
                             <li>
-                                <Typography variant="body1">{episode.name}</Typography>
+                                <Typography variant="body1">{episode.name || '-'}</Typography>
                             </li>
-                        </Link>
+                        </Link> : undefined
+
                     ))
                 }
             </ul>
